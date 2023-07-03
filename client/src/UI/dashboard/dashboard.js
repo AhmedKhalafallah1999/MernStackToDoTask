ec76b58ab25273278b4351dece4dbfe4358bc95b
Ahmed Khalaf <ahmed1999khalafallah@gmail.com>
2023-07-03 18:47:55 +0200

    Added Features

--- client/src/UI/dashboard/dashboard.js
+++ client/src/UI/dashboard/dashboard.js
@@ -20,41 +20,45 @@ const Dashboard = () => {
         <div className="addTask">
           <form>
             <input type="text" />
-            <button type="submit">Add</button>
           </form>
         </div>
 
+        <div className="singleTask first">
+          <div className="radioWithTask">
+            <div className="radioBtn"></div>
+            <div className="task">Clean the room</div>
+          </div>
+          <div className="closeTask">X</div>
+        </div>
         <div className="singleTask">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 close"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-
-          <p>Clean the room</p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="{1.5}"
-            stroke="currentColor"
-            className="w-6 h-6 close"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
+          <div className="radioWithTask">
+            <div className="radioBtn"></div>
+            <div className="task">Clean the room</div>
+          </div>
+          <div className="closeTask">X</div>
+        </div>
+        <div className="singleTask">
+          <div className="radioWithTask">
+            <div className="radioBtn"></div>
+            <div className="task">Clean the room</div>
+          </div>
+          <div className="closeTask">X</div>
+        </div>
+        <div className="singleTask">
+          <div className="radioWithTask">
+            <div className="radioBtn"></div>
+            <div className="task">Clean the room</div>
+          </div>
+          <div className="closeTask">X</div>
+        </div>
+        <div className="control">
+          <div>5 Items left</div>
+          <div className="status">
+            <span>All</span>
+            <span>Active</span>
+            <span>Completed</span>
+          </div>
+          <div>Clear Completed</div>
         </div>
       </div>
       {userInfo ? (
